bd10ed2be3eb324fb8397b750f468d6d114c0252
emirhanuzun <emirhanuzun98123@gmail.com>
2024-07-12 10:12:06 +0300

    feat/boo

--- client.c
+++ client.c
@@ -1,72 +1,3 @@
-/* #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <pthread.h>
-#include <unistd.h>
-#include <time.h>
-
-
-#define SOCK_PORT 4040
-#define THREAD_LIMIT 100
-
-int createConnection()
-{
-    int iClientFd = socket(AF_INET,SOCK_STREAM,0);
-    if(iClientFd < 0){
-        perror("Socket couldn't be created!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(SOCK_PORT);
-    socklen_t addrlen = sizeof(address);
-    if((inet_pton(AF_INET,"127.0.0.1",&address.sin_addr)) <= 0){
-        perror("inet_pton error!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int connectionStatus = connect(iClientFd,(struct sockaddr*)&address,addrlen);
-    if(connectionStatus < 0){
-        perror("Connection error!\n");
-        exit(EXIT_FAILURE);
-    }
-
-    return iClientFd;
-}
-
-void* runClient(void *args){
-    int iClientFd = createConnection();
-    char szSendMessage[100] = {0};
-    char szRecvMessage[1025] = {0};
-    int iRecvSize;
-    sprintf(&szSendMessage[0],"Hello, this is message from client %d\n",iClientFd);
-    send(iClientFd,&szSendMessage,strlen(szSendMessage),0);
-    while(1){
-        iRecvSize = recv(iClientFd,&szRecvMessage,1024,0);
-        if(iRecvSize == 0){
-            break;
-        }
-        printf("Reply from server : %s",szRecvMessage);
-    }
-    return NULL;
-}
-
-int main()
-{
-
-    pthread_t tempTask;
-    for(int i = 0;i<THREAD_LIMIT;i++){
-        if((pthread_create(&tempTask,NULL,&runClient,NULL)) < 0){
-            perror("Thread couldn't be created!\n");
-            exit(EXIT_FAILURE);
-        }
-    }
-    return 0;
-} */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -75,6 +6,7 @@ int main()
 #include <arpa/inet.h>
 #include <netinet/tcp.h>
 #include <pthread.h>
+#include <memory.h>
 #define THREAD_LIMIT 10
 
 int iaIndexes[THREAD_LIMIT];
@@ -114,9 +46,12 @@ void *sendMessageTask(void *args)
     int socketfd = connectSocket();
     char szMessageReceived[100] = {0};
     char szMessageSend[101] = {0};
+    int iMsgLen;
     for (int i = 0; i < 10000; i++)
     {
-        sprintf(szMessageSend, "Hello, this is message %d from Client %d!\n-", i, iSocketIndex);
+        sprintf((&szMessageSend[0])+4, "Hello, this is message %d from Client %d!\n", i, iSocketIndex);
+        iMsgLen = strlen(szMessageSend)-4;
+        memcpy(szMessageSend,()&iMsgLen,4);
         int iRet = send(socketfd, szMessageSend, strlen(szMessageSend), 0);
         if (iRet < 0)
         {
--- server.c
+++ server.c
@@ -11,56 +11,86 @@
 #define SOCK_PORT 4040
 #define ACTIVE_SOCKET_LIMIT 10
 
-struct timespec remaining1, request1 = { 0, 1 * 1000000 };
+struct timespec remaining1, request1 = {0, 1 * 1000000};
 
 int iaSocketsFd[ACTIVE_SOCKET_LIMIT] = {0};
 int iActiveSockNum = 0;
 int iEmptySockIndex = 0;
-//int iRequestNum = 0;
+// int iRequestNum = 0;
 
 pthread_mutex_t lock;
 
 void *handleConnectionTask(void *args)
 {
-    int iNewSocketFd = (int*)args;
+    int iNewSocketFd = (int)args;
     pthread_mutex_lock(&lock);
     iActiveSockNum++;
-    //iRequestNum++;
+    // iRequestNum++;
 
     char szRecvMessage[1025] = {0};
     char szBuffer[1025] = {0};
     int iRecvSize;
     int iBufferIndex = 0;
     int iRecvIndex = 0;
-    while(1){
-        iRecvSize = recv(iNewSocketFd,&szRecvMessage,1024,0);
-        if(iRecvSize == 0){
+    int iMessageLengthIndex = 0;
+    int iMessageLength = 0;
+    int iMessageReaded = 0;
+    while (1)
+    {
+        iRecvSize = recv(iNewSocketFd, &szRecvMessage, 1024,0);
+        printf("%d\n",iRecvSize);
+        if (iRecvSize == 0)
+        {
             printf("iRecvSize = 0\n");
             break;
         }
-        while(iRecvIndex != iRecvSize){
-            while(szRecvMessage[iRecvIndex] != '-'){
-                if(iRecvIndex == iRecvSize){
+        while (iRecvIndex != iRecvSize)
+        {
+            while (iMessageLengthIndex != 4)
+            {
+                iMessageLength = iMessageLength << 4;
+                iMessageLength = szRecvMessage[iRecvIndex];
+                iRecvIndex++;
+                iMessageLengthIndex++;
+                if (iRecvIndex == iRecvSize)
+                {
+                    break;
+                }
+            }
+            if (iRecvIndex == iRecvSize)
+            {
+                break;
+            }
+            while (iMessageReaded != iMessageLength)
+            {
+                if (iRecvIndex == iRecvSize)
+                {
                     break;
                 }
                 szBuffer[iBufferIndex] = szRecvMessage[iRecvIndex];
                 iRecvIndex++;
-                iBufferIndex++;
+                iBufferIndex++,
+                iMessageReaded++;
             }
-            if(iRecvIndex == iRecvSize){
+            if (iRecvIndex == iRecvSize)
+            {
                 break;
             }
             szBuffer[iBufferIndex] = 0;
-            iRecvIndex++;
-            printf("Message from client %d : %s",iNewSocketFd,szBuffer);
+            printf("Message from client %d : %s", iNewSocketFd, szBuffer);
+            iMessageLengthIndex = 0;
+            iMessageReaded = 0;
             iBufferIndex = 0;
-            memset(szBuffer,0,1025);
+            iMessageLength = 0;
+            memset(szBuffer, 0, 1025);
         }
         iRecvIndex = 0;
     }
-    
-    for(int i = 0;i<ACTIVE_SOCKET_LIMIT;i++){
-        if(iNewSocketFd == iaSocketsFd[i]){
+
+    for (int i = 0; i < ACTIVE_SOCKET_LIMIT; i++)
+    {
+        if (iNewSocketFd == iaSocketsFd[i])
+        {
             iaSocketsFd[i] = 0;
             break;
         }
@@ -77,59 +107,70 @@ void *handleConnectionTask(void *args)
 int main()
 {
     int iServerFd = socket(AF_INET, SOCK_STREAM, 0);
-    if(iServerFd < 0){
+    if (iServerFd < 0)
+    {
         perror("Socket couldn't be created!\n");
         exit(EXIT_FAILURE);
     }
     struct sockaddr_in address;
     address.sin_family = AF_INET;
-    address.sin_port = htons(SOCK_PORT); 
+    address.sin_port = htons(SOCK_PORT);
     address.sin_addr.s_addr = INADDR_ANY;
     socklen_t addrlen = sizeof(address);
 
     int iSockOpt = 1;
-    if(setsockopt(iServerFd,SOL_SOCKET,SO_REUSEADDR,&iSockOpt,sizeof(iSockOpt)) < 0){
+    if (setsockopt(iServerFd, SOL_SOCKET, SO_REUSEADDR, &iSockOpt, sizeof(iSockOpt)) < 0)
+    {
         perror("setsockopt failed!\n");
         exit(EXIT_FAILURE);
     }
 
-    if((bind(iServerFd,(struct sockaddr*)&address,addrlen)) < 0){
+    if ((bind(iServerFd, (struct sockaddr *)&address, addrlen)) < 0)
+    {
         perror("Socket couldn't be binded!\n");
         exit(EXIT_FAILURE);
     }
 
-    if(listen(iServerFd,100) < 0){
+    if (listen(iServerFd, 100) < 0)
+    {
         perror("Socket couldn't be listened!\n");
         exit(EXIT_FAILURE);
     }
 
     int iNewSocketFd;
     pthread_t tempTask;
-    while(1){
-        while(1){
-            if(iActiveSockNum < 10){
+    while (1)
+    {
+        while (1)
+        {
+            if (iActiveSockNum < 10)
+            {
                 break;
             }
             printf("Connection waiting!\n");
             nanosleep(&request1, &remaining1);
         }
-        for(int i = 0;i< ACTIVE_SOCKET_LIMIT;i++){
-            if(iaSocketsFd[i] == 0){
+        for (int i = 0; i < ACTIVE_SOCKET_LIMIT; i++)
+        {
+            if (iaSocketsFd[i] == 0)
+            {
                 iEmptySockIndex = i;
                 break;
             }
         }
-        iaSocketsFd[iEmptySockIndex] = accept(iServerFd,(struct sockaddr*)&address,&addrlen);
-        if(iaSocketsFd[iEmptySockIndex] < 0){
+        iaSocketsFd[iEmptySockIndex] = accept(iServerFd, (struct sockaddr *)&address, &addrlen);
+        if (iaSocketsFd[iEmptySockIndex] < 0)
+        {
             perror("Connection couldn't be accepted!\n");
             exit(EXIT_FAILURE);
         }
-        if((pthread_create(&tempTask,NULL,handleConnectionTask,(void *)iaSocketsFd[iEmptySockIndex]) )!= 0){
+        long l = (long)iaSocketsFd[iEmptySockIndex];
+        if ((pthread_create(&tempTask, NULL, handleConnectionTask, (void *)l)) != 0)
+        {
             perror("Thread couldn't be created!\n");
             exit(EXIT_FAILURE);
         }
     }
-
 
     shutdown(iServerFd, SHUT_RDWR);
     close(iServerFd);
